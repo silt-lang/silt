eaae2a9775f94ba4eeb5499f5cfe02ce09900341
Harlan Haskins <harlan@harlanhaskins.com>
2017-10-22 23:05:54 -0400

    Prettify syntax dumper output

--- Sources/Drill/Invocation.swift
+++ Sources/Drill/Invocation.swift
@@ -7,55 +7,58 @@
 
 import Foundation
 import Lithosphere
+import Rainbow
 import Crust
 
 extension Diagnostic.Message {
-    static let noInputFiles = Diagnostic.Message(.error, "no input files provided")
+  static let noInputFiles = Diagnostic.Message(.error, "no input files provided")
 }
 
 public struct Invocation {
-    public let options: Options
-    public let sourceFiles: [String]
+  public let options: Options
+  public let sourceFiles: [String]
 
-    public init(options: Options, paths: [String]) {
-        self.options = options
-        self.sourceFiles = paths
+  public init(options: Options, paths: [String]) {
+    self.options = options
+    self.sourceFiles = paths
+  }
+
+  public func run() throws {
+    let engine = DiagnosticEngine()
+    let consumer = PrintingDiagnosticConsumer(stream: &stderrStream)
+    engine.register(consumer)
+
+    Rainbow.enabled = options.colorsEnabled
+
+    if sourceFiles.isEmpty {
+      engine.diagnose(.noInputFiles)
+      return
     }
 
-    public func run() throws {
-        let engine = DiagnosticEngine()
-        let consumer = PrintingDiagnosticConsumer(stream: &stderrStream)
-        engine.register(consumer)
-
-        if sourceFiles.isEmpty {
-            engine.diagnose(.noInputFiles)
-            return
+    for path in sourceFiles {
+      let url = URL(fileURLWithPath: path)
+      let contents = try String(contentsOf: url, encoding: .utf8)
+      let lexer = Lexer(input: contents, filePath: path)
+      let tokens = lexer.tokenize()
+      switch options.mode {
+      case .compile:
+        fatalError("only Parse is implemented")
+      case .dump(.tokens):
+        TokenDescriber.describe(tokens, to: &stdoutStream)
+      case .dump(.file):
+        for token in tokens {
+          token.writeSourceText(to: &stdoutStream, includeImplicit: false)
         }
-
-        for path in sourceFiles {
-            let url = URL(fileURLWithPath: path)
-            let contents = try String(contentsOf: url, encoding: .utf8)
-            let lexer = Lexer(input: contents, filePath: path)
-            let tokens = lexer.tokenize()
-            switch options.mode {
-            case .compile:
-              fatalError("only Parse is implemented")
-            case .dump(.tokens):
-              TokenDescriber.describe(tokens, to: &stdoutStream)
-            case .dump(.file):
-              for token in tokens {
-                token.writeSourceText(to: &stdoutStream, includeImplicit: false)
-              }
-            case .dump(.shined):
-              let layoutTokens = layout(tokens)
-              for token in layoutTokens {
-                token.writeSourceText(to: &stdoutStream, includeImplicit: true)
-              }
-            case .dump(.parse):
-              let layoutTokens = layout(tokens)
-              let parser = Parser(tokens: layoutTokens)
-              SyntaxDumper(stream: &stderrStream).dump(parser.parseTopLevelModule()!)
-            }
+      case .dump(.shined):
+        let layoutTokens = layout(tokens)
+        for token in layoutTokens {
+          token.writeSourceText(to: &stdoutStream, includeImplicit: true)
         }
+      case .dump(.parse):
+        let layoutTokens = layout(tokens)
+        let parser = Parser(tokens: layoutTokens)
+        SyntaxDumper(stream: &stderrStream).dump(parser.parseTopLevelModule()!)
+      }
     }
+  }
 }
--- Sources/Lithosphere/SyntaxDumper.swift
+++ Sources/Lithosphere/SyntaxDumper.swift
@@ -5,6 +5,9 @@
 /// This project is released under the MIT license, a copy of which is
 /// available in the repository.
 
+import Foundation
+import Rainbow
+
 public class SyntaxDumper<StreamType: TextOutputStream> {
   var stream: StreamType
   var indent = 0
@@ -24,25 +27,48 @@ public class SyntaxDumper<StreamType: TextOutputStream> {
     stream.write(string)
   }
 
-  public func line(_ string: String = "") {
-    write(string)
+  public func line(_ string: String? = nil) {
+    if let string = string { write(string) }
     stream.write("\n")
   }
 
+  public func writeLoc(_ loc: SourceLocation?) {
+    if let loc = loc {
+      let url = URL(fileURLWithPath: loc.file)
+      stream.write(" ")
+      stream.write(url.lastPathComponent.yellow)
+      stream.write(":")
+      stream.write("\(loc.line)".cyan)
+      stream.write(":")
+      stream.write("\(loc.column)".cyan)
+    }
+  }
+
   public func dump(_ node: Syntax, root: Bool = true) {
+    write("(")
     switch node {
     case let node as TokenSyntax:
-      write("(token .\(node.tokenKind))")
+      switch node.tokenKind {
+      case .identifier(let name):
+        stream.write("identifier".green.bold)
+        stream.write(" \"\(name)\"".red)
+      default:
+        stream.write("\(node.tokenKind)".green.bold)
+      }
+      writeLoc(node.startLoc)
     default:
-      write("(\(node.raw.kind)")
+      stream.write("\(node.raw.kind)".magenta.bold)
+      writeLoc(node.startLoc)
       withIndent {
         for child in node.children {
           line()
           dump(child, root: false)
         }
       }
-      stream.write(")")
     }
-    if root { line() }
+    stream.write(")")
+    if root {
+      line()
+    }
   }
 }
