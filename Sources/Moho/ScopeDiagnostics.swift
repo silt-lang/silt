fbdf6a491b63d0ac296d718c34ad46359a507d61
Harlan Haskins <harlan@harlanhaskins.com>
2017-12-31 16:33:16 -0500

    Update copyright dates

--- Sources/Moho/ScopeDiagnostics.swift
+++ Sources/Moho/ScopeDiagnostics.swift
@@ -1,6 +1,6 @@
 /// ScopeDiagnostics.swift
 ///
-/// Copyright 2017, The Silt Language Project.
+/// Copyright 2017-2018, The Silt Language Project.
 ///
 /// This project is released under the MIT license, a copy of which is
 /// available in the repository.
